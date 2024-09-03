bfbdb19074c2dd9eaaf35ad23b0737574b70d646
asilbeksagdullayev <asilbek1188akeng@gmail.com>
2024-09-03 03:00:55 -0700

    true version

--- index.js
+++ index.js
@@ -2,25 +2,26 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 	res.writeHead(200, { 'Content-Type': 'text/html' });
-	if (res.method === 'GET') {
+	if (req.method === 'GET') {  // Use req.method instead of res.method
 		res.end(`
 <h3>Send name</h3>
 <form method="post" action="/">
-<input type="name" name="Name" placeholder="Enter your name " />
-<button type="text">Send Name</button>
+<input type="text" name="Name" placeholder="Enter your name" />
+<button type="submit">Send Name</button> <!-- Corrected type to 'submit' -->
 </form>
-  `);
-	} else if (res.method === 'POST') {
+	`);
+	} else if (req.method === 'POST') {
 		const name = [];
 		req.on('data', (data) => {
-			name.push(Buffer.from(data));
+			name.push(data);
 		});
 		req.on('end', () => {
-			const message = name.toString().split('=')[1];
-			res.end(`Name was success added : ${message}`);
+			const message = Buffer.concat(name).toString().split('=')[1];
+			res.end(`Name was successfully added: ${message}`);
 		});
 	}
 });
+
 server.listen(3000, () => {
-	console.log('Server was success started !');
+	console.log('Server was successfully started!');
 });
